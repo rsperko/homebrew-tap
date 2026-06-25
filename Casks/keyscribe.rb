400e2bdd18784370ccb274109edf8d3075c881db
rsperko <rick@sperko.com>
2026-06-25 08:44:04 -0500

    keyscribe 0.1.0

---
+++ Casks/keyscribe.rb
@@ -0,0 +1,23 @@
+cask "keyscribe" do
+  version "0.1.0"
+  sha256 "072e1adad8194e58b15aca5c907e09f531a172a03a604e84fc1461e35fc0756c"
+
+  url "https://github.com/rsperko/keyscribe/releases/download/v#{version}/KeyScribe-#{version}.dmg"
+  name "KeyScribe"
+  desc "Privacy-first, local-first voice dictation for macOS"
+  homepage "https://github.com/rsperko/keyscribe"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :tahoe"
+
+  app "KeyScribe.app"
+
+  zap trash: [
+    "~/Library/Application Support/KeyScribe",
+    "~/Library/Preferences/com.keyscribe.app.plist",
+  ]
+end
