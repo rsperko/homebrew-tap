cac11cb460b45a770bcab6457492b91185bbe064
rsperko <rick@sperko.com>
2026-07-07 11:39:36 -0500

    keyscribe 0.2.1

--- Casks/keyscribe.rb
+++ Casks/keyscribe.rb
@@ -1,6 +1,6 @@
 cask "keyscribe" do
-  version "0.2.0"
-  sha256 "1f2f6e9b88dbdc606d9b7be0f5561c07d8af032e3bf6fbfb2df206181f7d7641"
+  version "0.2.1"
+  sha256 "55459e02d186786d65b1d50a38bb5cc113708205e67f5b614e9d301889afae36"
 
   url "https://github.com/rsperko/keyscribe/releases/download/v#{version}/KeyScribe-#{version}.dmg"
   name "KeyScribe"
